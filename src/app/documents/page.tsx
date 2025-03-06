8e77d4ca6db1ac1e713c57341d60a351b9f85d04
Bohdan <werewtyu23@gmail.com>
2025-03-06 22:36:10 +0200

    Tiptap editor setup

---
+++ src/app/documents/page.tsx
@@ -0,0 +1,5 @@
+const DocumentsPage = () => {
+    return ( <div>Documents page here</div> );
+}
+ 
+export default DocumentsPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Home = () => {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <Button>click</Button>
+      click <Link href="/documents/123" className="text-blue-400">here</Link> to go to the documents 123 page
     </div>
   );
 }
