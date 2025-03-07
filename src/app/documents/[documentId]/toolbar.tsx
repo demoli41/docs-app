371e0ac7a02c8b7e66ebca1f764b8a6dbac0c259
Bohdan <werewtyu23@gmail.com>
2025-03-08 00:27:36 +0200

    Textcolor and highlight color tools

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -9,9 +9,60 @@ import {
     DropdownMenuItem,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import {type ColorResult,SketchPicker} from "react-color";
 import {type Level} from "@tiptap/extension-heading";
-import { BoldIcon, ChevronDownIcon, ItalicIcon, ListTodoIcon, LucideIcon, MessageSquarePlusIcon, PrinterIcon, Redo2Icon, RemoveFormattingIcon, SpellCheckIcon, UnderlineIcon, Undo2Icon } from "lucide-react";
+import { BoldIcon, ChevronDownIcon, HighlighterIcon, ItalicIcon, ListTodoIcon, LucideIcon, MessageSquarePlusIcon, PrinterIcon, Redo2Icon, RemoveFormattingIcon, SpellCheckIcon, UnderlineIcon, Undo2Icon } from "lucide-react";
 
+const HighlightColorButton=()=>{
+    const {editor}=useEditorStore();
+
+    const value=editor?.getAttributes("highlight").color || "#FFFFFF";
+
+    const onChange=(color:ColorResult)=>{
+        editor?.chain().focus().setHighlight({color:color.hex}).run();
+    };
+
+    return(
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <button
+                 className="h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm">
+                <HighlighterIcon className="size-4"/>
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="p-0">
+                    <SketchPicker 
+                    color={value}
+                    onChange={onChange}/>
+                </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
+
+const TextColorButton=()=>{
+    const {editor}=useEditorStore();
+
+    const value=editor?.getAttributes("textStyle").color || "#000000";
+
+    const onChange=(color:ColorResult)=>{
+        editor?.chain().focus().setColor(color.hex).run();
+    };
+
+    return(
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <button
+                 className="h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm">
+                    <span className="text-xs">A</span>
+                    <div className="h-0.5 w-full" style={{backgroundColor:value}}/>
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="p-0">
+                    <SketchPicker color={value} onChange={onChange}/>
+                </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
 
 const HeadingLevelButton=()=>{
     const {editor}=useEditorStore();
@@ -246,8 +297,8 @@ export const Toolbar = () => {
                 {...item}
             />
         ))}
-        {/*TODO: Text color*/}
-        {/*TODO: Highligt color*/}
+        <TextColorButton/>
+        <HighlightColorButton/>
         <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
         {/*TODO: Link*/}
         {/*TODO: Image*/}
