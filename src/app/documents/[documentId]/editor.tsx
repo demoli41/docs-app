92c4672178196749bec999889ff36f5ca2edc639
Bohdan <werewtyu23@gmail.com>
2025-03-09 16:00:43 +0200

    Margin ruler component

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -22,6 +22,7 @@ import { FontSizeExtension } from '@/extensions/font-size';
 import {LineHeightExtension} from '@/extensions/line-height';
 
 import { useEditorStore } from '@/store/use-editor-store';
+import { Ruler } from './ruler';
 
 
 export const Editor = () => {
@@ -29,6 +30,7 @@ export const Editor = () => {
         const {setEditor} = useEditorStore();
 
         const editor = useEditor({
+            immediatelyRender: false,
             onCreate({editor}) {
                 setEditor(editor);
             },
@@ -112,7 +114,8 @@ export const Editor = () => {
 
     return ( 
         <div className='size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible'>
-            <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
+           <Ruler/>
+           <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
                 <EditorContent editor={editor} />
             </div>
         </div>
