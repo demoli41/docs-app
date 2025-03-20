35de4944d622750f0c9aae70e9c63a7dcdec7957
Bohdan <werewtyu23@gmail.com>
2025-03-20 22:35:12 +0200

    Margin-ruler improvement, collaborative margin

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -23,6 +23,7 @@ import { FontSizeExtension } from '@/extensions/font-size';
 import {LineHeightExtension} from '@/extensions/line-height';
 
 import { useEditorStore } from '@/store/use-editor-store';
+import { useStorage } from '@liveblocks/react';
 import { Ruler } from './ruler';
 import { Threads } from './threads';
 
@@ -30,6 +31,9 @@ import { Threads } from './threads';
 export const Editor = () => {
 
         const liveblocks = useLiveblocksExtension();
+
+        const leftMargin = useStorage((root) => root.leftMargin);
+        const rightMargin = useStorage((root) => root.rightMargin);
 
         const {setEditor} = useEditorStore();
 
@@ -61,7 +65,7 @@ export const Editor = () => {
             },
         editorProps: {
             attributes: {
-                style:"padding-left:56px; padding:56px;",
+                style:`padding-left:${leftMargin ?? 56}px; padding-right:${rightMargin ?? 56}px;`,
                 class:"focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor:text"
             },
             },
