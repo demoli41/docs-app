c0f41881a0c25e23d5ad1cc9a9e5ca2224133544
Bohdan <werewtyu23@gmail.com>
2025-03-20 22:18:00 +0200

    Inbox component, notifications feature

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -21,6 +21,7 @@ import { BsFilePdf } from 'react-icons/bs';
 import { useEditorStore } from '@/store/use-editor-store';
 import { OrganizationSwitcher, UserButton } from '@clerk/nextjs';
 import { Avatars } from './avatars';
+import { Inbox } from './inbox';
 
 export const Navbar = () => {
    /* const [canUndo, setCanUndo] = useState(false);
@@ -236,6 +237,7 @@ export const Navbar = () => {
             </div>
             <div className="flex gap-3 items-center pl-6">
                     <Avatars/>
+                    <Inbox/>
                     <OrganizationSwitcher
                     afterCreateOrganizationUrl="/"
                     afterLeaveOrganizationUrl="/"
